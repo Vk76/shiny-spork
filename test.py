0da2624936df9c7cdb2d5396069b0961eff91bdf
Vishal Kriplani <48404770+Vk76@users.noreply.github.com>
2021-07-18 14:19:15 +0530

    Update test.py
    fix : bug-fix in test

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
 def my_sum(a, b):
+	if a == None or b == None:
+		raise ValueError("Input values can't be null")
 	return 2*a + 3*b
 
 def my_mult(a, b):
+	if a == None or b == None:
+		raise ValueError("Input values can't be null")
 	return a*b*2
 
 print(my_sum(5,6))
